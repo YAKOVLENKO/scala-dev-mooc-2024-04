5cd9cdf30abad5388f56f539076bc4594f24efca
A.M <svobodinayakovlenko@yandex.ru>
2024-07-21 16:34:50 +0300

    Revert "HW-3: implement Wrap + done exercises"
    This reverts commit b3256882ac0008abfeab3ea9a6a1968e5f2081ac.

--- src/main/scala/monad/monad.scala
+++ src/main/scala/monad/monad.scala
@@ -9,21 +9,19 @@ package object monad {
 
     def get: A
 
-    // todo А это точно должно быть в trait, а не в object?
-    def pure[R](x: R): Wrap[R] = NonEmptyWrap(x)
+    def pure[R](x: R): Wrap[R] = ???
 
     def flatMap[R](f: A => Wrap[R]): Wrap[R] = {
-      f(get)
+      ???
     }
 
     // HINT: map можно реализовать через pure и flatMap
     def map[R](f: A => R): Wrap[R] = {
-      flatMap(f.andThen(pure))
+      ???
     }
 
     def withFilter(f: A => Boolean): Wrap[A] = {
-      if (f(get)) pure(get)
-      else EmptyWrap
+      ???
     }
 
   }
