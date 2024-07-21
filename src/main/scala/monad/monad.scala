5a3e379c64bcb4c7550d15634efb0616cedbabae
A.M <svobodinayakovlenko@yandex.ru>
2024-07-21 16:22:41 +0300

    HW-3: implement Wrap + done exercises

--- src/main/scala/monad/monad.scala
+++ src/main/scala/monad/monad.scala
@@ -9,19 +9,21 @@ package object monad {
 
     def get: A
 
-    def pure[R](x: R): Wrap[R] = ???
+    // todo А это точно должно быть в trait, а не в object?
+    def pure[R](x: R): Wrap[R] = NonEmptyWrap(x)
 
     def flatMap[R](f: A => Wrap[R]): Wrap[R] = {
-      ???
+      f(get)
     }
 
     // HINT: map можно реализовать через pure и flatMap
     def map[R](f: A => R): Wrap[R] = {
-      ???
+      flatMap(f.andThen(pure))
     }
 
     def withFilter(f: A => Boolean): Wrap[A] = {
-      ???
+      if (f(get)) pure(get)
+      else EmptyWrap
     }
 
   }
